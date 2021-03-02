9c81e1e1c086958ae5c7c35d2c57ed39355a32bf
Rivikid <rivikid@gmail.com>
2021-03-02 20:52:07 +0100

    Getting Local Data (cities, countries JSONs).

---
+++ modules/weatherForecast/config.js
@@ -0,0 +1,5 @@
+/* api */
+const citiesEndpoint = "modules/weatherForecast/json/city.list.json";
+const countriesEndpoint = "modules/weatherForecast/json/countries.json";
+
+export { citiesEndpoint, countriesEndpoint };
--- modules/weatherForecast/weatherForecast.js
+++ modules/weatherForecast/weatherForecast.js
@@ -1,8 +1,20 @@
 import ModuleStructure from "./components/moduleStructure.js";
+import Loader from "./components/loader.js";
+import { citiesEndpoint, countriesEndpoint } from "./config.js";
+import { getLocalData } from "./services/localDataService.js";
 
 export function WeatherForecast(moduleEl) {
   // inserting structure of the Weather forecast module
   moduleEl.innerHTML = ModuleStructure();
 
-  console.log("Weather Forecast loaded.");
+  // show loader until local data loaded
+  const loaderElement = moduleEl.querySelector(".weather__loader");
+  loaderElement.innerHTML = Loader();
+
+  // fetching data from local JSONs
+  getLocalData(citiesEndpoint, countriesEndpoint).then((localData) => {
+    loaderElement.innerHTML = "";
+    console.log(localData);
+    console.log("Weather Forecast loaded.");
+  });
 }
